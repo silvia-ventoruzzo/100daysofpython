77a3edd9f637aadcb97771bc5bb1e4387c62f261
silvia-ventoruzzo <silvia.ventoruzzo@gmail.com>
2023-06-24 15:01:07 +0200

    fix day 48 project

--- day48_game_playing_bot/project/main.py
+++ day48_game_playing_bot/project/main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException
 import time
 
 chrondriver_path = '../chromedriver_mac64/chromedriver'
@@ -9,13 +10,13 @@ driver = webdriver.Chrome(service=service)
 url = 'http://orteil.dashnet.org/experiments/cookie/'
 driver.get(url)
 
-# Click on cookie
 cookie = driver.find_element(By.ID, "cookie")
 
 click_wait_time = time.time() + 5
 end_of_game_time = time.time() + 60*5
 
 while True:
+    # Click on cookie
     cookie.click()
     
     # Every 5 seconds:
@@ -23,26 +24,22 @@ while True:
 
         # Available ones have class="", while non available ones have class="grayed"
         add_ons = driver.find_elements(By.CSS_SELECTOR, '#store [class=""]')
-        available_add_ons_ids = [element.get_attribute("id") for element in add_ons]
 
-        # Get price
-        prices = []
-        for id in available_add_ons_ids:
-            price = driver.find_element(By.CSS_SELECTOR, f'#{id} b')
-            prices.append(int(''.join(filter(str.isdigit, price.text))))
-
-        if len(prices) > 0:
-            max_price = prices.index(max(prices))
-            add_on_to_buy_id = available_add_ons_ids[max_price]
+        # Click on highest level add on available
+        try:
+            available_add_ons_ids = [element.get_attribute("id") for element in add_ons]
+            add_on_to_buy_id = available_add_ons_ids[-1]
             add_on_to_buy = driver.find_element(By.ID, add_on_to_buy_id)
             add_on_to_buy.click()
+        except Exception:
+            pass
 
-# After 5 minutes stop the bot and check the cookies per second
-    if time.time() > end_of_game_time:
-        # Get cookies per second value
-        cps = driver.find_element(By.ID, "cps")
-        # cps = float(cps.text.split(':')[1].strip())
-        print(cps.text)
-        break
+        # After 5 minutes stop the bot and check the cookies per second
+        if time.time() > end_of_game_time:
+            # Get cookies per second value
+            cps = driver.find_element(By.ID, "cps")
+            # cps = float(cps.text.split(':')[1].strip())
+            print(cps.text)
+            break
 
 driver.quit()
